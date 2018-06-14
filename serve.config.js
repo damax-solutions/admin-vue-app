b71980e7d7169bdc38ebbb5029d429636ab9637c
Dmitri Lakachauskis <lakiboy83@gmail.com>
2018-06-14 15:56:20 +0300

    Enable hot setting.

--- serve.config.js
+++ serve.config.js
@@ -1,18 +1,26 @@
+const os = require('os')
 const { resolve } = require('path')
 
 const history = require('connect-history-api-fallback')
 const convert = require('koa-connect')
 
 // Plugins
-const webpackServeWaitpage = require('webpack-serve-waitpage')
+const webpackServeWaitPage = require('webpack-serve-waitpage')
+
+const host = os.networkInterfaces().eth0[0]['address']
 
 module.exports = {
+    host,
+
     clipboard: false,
-    host: '0.0.0.0',
     content: resolve(__dirname, 'build'),
 
-    add: (app, middleware, options) => {
-        app.use(webpackServeWaitpage(options))
+    hot: {
+        host: { server: host, client: 'localhost' }
+    },
+
+    add (app, middleware, options) {
+        app.use(webpackServeWaitPage(options))
         app.use(convert(history()))
 
         // middleware.webpack()
